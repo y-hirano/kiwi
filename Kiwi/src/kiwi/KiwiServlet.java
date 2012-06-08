ea812d870c4ecdcc04a7f73e90e97347ab2b34f9
yuta <yuta@luna>
2012-06-08 15:23:01 +0900

    Untrack libraries and fix the appengine settings.

--- Kiwi/src/kiwi/KiwiServlet.java
+++ Kiwi/src/kiwi/KiwiServlet.java
@@ -5,8 +5,7 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class KiwiServlet extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
         resp.getWriter().println("Hello, world");
     }
